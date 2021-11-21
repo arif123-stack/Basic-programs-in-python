527d20719d8f4b27c27bf0a7bfc66095e69b1404
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-11-21 23:02:12 +0530

    add program

--- Basic Programs in Python.py
+++ Basic Programs in Python.py
@@ -64,6 +64,16 @@ import random
 
 print(random.randint(0,9))
 
+# Taking kilometers input from the user
+kilometers = float(input("Enter value in kilometers: "))
+
+# conversion factor
+conv_fac = 0.621371
+
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
+
 
 
 
