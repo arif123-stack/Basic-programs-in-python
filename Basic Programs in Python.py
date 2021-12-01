1631604142ef54648de4720cd648d978d51f0e05
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-12-01 23:01:43 +0530

    add new program

--- Basic Programs in Python.py
+++ Basic Programs in Python.py
@@ -74,7 +74,14 @@ conv_fac = 0.621371
 miles = kilometers * conv_fac
 print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
 
+# Python Program to convert temperature in celsius to fahrenheit
 
+# change this value for a different result
+celsius = 37.5
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
 
 
 
