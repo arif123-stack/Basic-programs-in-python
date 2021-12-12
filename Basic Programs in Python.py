e4b431876e4d86ff26fa55c78ec63e4b3c37cd40
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-12-12 20:31:27 +0530

    add a new program

--- Basic Programs in Python.py
+++ Basic Programs in Python.py
@@ -143,3 +143,13 @@ if num == sum:
    print(num,"is an Armstrong number")
 else:
    print(num,"is not an Armstrong number")
+# Multiplication table (from 1 to 10) in Python
+
+num = 12
+
+# To take input from the user
+# num = int(input("Display multiplication table of? "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
