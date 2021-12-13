22915c868ecf9d3f9b759a33644339efc91c4573
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-12-13 22:05:03 +0530

    add new program

--- Basic Programs in Python.py
+++ Basic Programs in Python.py
@@ -179,3 +179,23 @@ else:
        n1 = n2
        n2 = nth
        count += 1
+# Python program to check if the number is an Armstrong number or not
+
+# take input from the user
+num = int(input("Enter a number: "))
+
+# initialize sum
+sum = 0
+
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
