f245a70788cd2621ca6d7ae080f36336639c7be4
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-12-14 22:09:57 +0530

    add new program

--- Basic Programs in Python.py
+++ Basic Programs in Python.py
@@ -199,3 +199,17 @@ if num == sum:
    print(num,"is an Armstrong number")
 else:
    print(num,"is not an Armstrong number")
+# Sum of natural numbers up to num
+
+num = 16
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate until zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
+
