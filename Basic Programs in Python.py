e7073fa0c9600ee31246137c225b5bdd4a343251
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-12-15 22:57:41 +0530

    add new program

--- Basic Programs in Python.py
+++ Basic Programs in Python.py
@@ -213,3 +213,17 @@ else:
        num -= 1
    print("The sum is", sum)
 
+# Display the powers of 2 using anonymous function
+
+terms = 10
+
+# Uncomment code below to take input from the user
+# terms = int(input("How many terms? "))
+
+# use anonymous function
+result = list(map(lambda x: 2 ** x, range(terms)))
+
+print("The total terms are:",terms)
+for i in range(terms):
+   print("2 raised to power",i,"is",result[i])
+
