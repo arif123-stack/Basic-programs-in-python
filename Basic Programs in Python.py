514531b6bebcd35042af62b594319df04605e51d
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-12-16 22:03:12 +0530

    add new program

--- Basic Programs in Python.py
+++ Basic Programs in Python.py
@@ -227,3 +227,12 @@ print("The total terms are:",terms)
 for i in range(terms):
    print("2 raised to power",i,"is",result[i])
 
+# Take a list of numbers
+my_list = [12, 65, 54, 39, 102, 339, 221,]
+
+# use anonymous function to filter
+result = list(filter(lambda x: (x % 13 == 0), my_list))
+
+# display the result
+print("Numbers divisible by 13 are",result)
+
