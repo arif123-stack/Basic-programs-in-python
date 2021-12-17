dd47d12d3ac385322ffe7804dbdf01b720b56c52
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-12-17 22:13:38 +0530

    add new program

--- Basic Programs in Python.py
+++ Basic Programs in Python.py
@@ -236,3 +236,11 @@ result = list(filter(lambda x: (x % 13 == 0), my_list))
 # display the result
 print("Numbers divisible by 13 are",result)
 
+# Python program to convert decimal into other number systems
+dec = 344
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
+
