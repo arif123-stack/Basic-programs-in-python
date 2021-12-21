ccccc713fcf2a3879e428bebde1bcd80a135d7b7
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-12-21 23:03:00 +0530

    add new program

--- Basic Programs in Python.py
+++ Basic Programs in Python.py
@@ -290,3 +290,15 @@ num1 = 54
 num2 = 24
 
 print("The L.C.M. is", compute_lcm(num1, num2))
+# Python Program to find the factors of a number
+
+# This function computes the factor of the argument passed
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = 320
+
+print_factors(num)
