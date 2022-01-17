2a8c83de2ffcfe0c292d8f28e9ba51bffb6c539d
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2022-01-17 14:02:07 +0530

    add new program

--- Basic Programs in Python.py
+++ Basic Programs in Python.py
@@ -671,3 +671,14 @@ for i in range(rows):
     for j in range(i+1):
         print(j+1, end=" ")
     print("\n")
+rows = int(input("Enter number of rows: "))
+
+ascii_value = 65
+
+for i in range(rows):
+    for j in range(i+1):
+        alphabet = chr(ascii_value)
+        print(alphabet, end=" ")
+    
+    ascii_value += 1
+    print("\n")
