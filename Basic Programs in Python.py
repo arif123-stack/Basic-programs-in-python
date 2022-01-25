bc7fb9fecb8bcc443f00c67a2fc729a64468e33c
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2022-01-25 16:37:21 +0530

    add new program

--- Basic Programs in Python.py
+++ Basic Programs in Python.py
@@ -733,3 +733,17 @@ for i in range(1, rows+1):
     
     count1 = count = k = 0
     print()
+rows = int(input("Enter number of rows: "))
+
+k = 0
+
+for i in range(1, rows+1):
+    for space in range(1, (rows-i)+1):
+        print(end="  ")
+   
+    while k!=(2*i-1):
+        print("* ", end="")
+        k += 1
+   
+    k = 0
+    print()
