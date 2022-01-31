141c98bb6c1b9780dfd8e625498a1ea6dfff77a3
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2022-01-31 14:02:36 +0530

    add new program

--- Basic Programs in Python.py
+++ Basic Programs in Python.py
@@ -779,3 +779,16 @@ for i in range(rows, 1, -1):
     for j in range(1, i-1):
         print("* ", end="")
     print()
+rows = int(input("Enter number of rows: "))
+coef = 1
+
+for i in range(1, rows+1):
+    for space in range(1, rows-i+1):
+        print(" ",end="")
+    for j in range(0, i):
+        if j==0 or i==0:
+            coef = 1
+        else:
+            coef = coef * (i - j)//j
+        print(coef, end = " ")
+    print()
